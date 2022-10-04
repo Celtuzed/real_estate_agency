67f475cf2fe4a36f6b6f8a908c4b3ab9e64bf35b
Celtuzed <uter222222@gmail.com>
2022-10-04 19:15:46 +0300

    Add Owner model

--- property/models.py
+++ property/models.py
@@ -77,3 +77,15 @@ class Complaint(models.Model):
 
     def __str__(self):
         return f'{self.user}, {self.user}'
+
+
+class Owner(models.Model):
+    owner = models.CharField('ФИО владельца', max_length=200)
+    owners_phonenumber = models.CharField('Номер владельца', max_length=20)
+    owner_pure_phonenumber = PhoneNumberField('Нормализованный номер владельца', blank=True)
+    flat = models.ForeignKey(
+        Flat,
+        'Квартиры, в собственности пользователя',
+        related_name='user'
+    )
+    
